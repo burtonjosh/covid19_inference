4306d5806ad0f1ca61c7eef8b05a21de2a4f830b
Josh Burton <josh@burton-online.me.uk>
2020-04-12 23:54:55 +0100

    writing simple manifold mala and tests

--- src/covid_inference.py
+++ src/covid_inference.py
@@ -242,27 +242,37 @@ class hybrid_rosenbrock23:
                                             2*self.shape[4]*(position[3]**2 -position[4])])
         return log_likelihood_gradient
 
-# TODO:
-    # def log_likelihood_hessian(self,position):
-    #     """
-    #     A function returning the Hessian of the log likelihood of the multivariate normal distribution
-    #     for a given position in a Markov chain.
-    #
-    #     Parameters
-    #     ----------
-    #
-    #     position : numpy array
-    #         the current position in the Markov chain.
-    #
-    #     Returns
-    #     -------
-    #
-    #     log_likelihood_hessian : double
-    #         the Hessian of the log of the multivariate normal likelihood function.
-    #
-    #     """
-    #     log_likelihood_hessian = -self.covariance_matrix_inverse
-    #     return log_likelihood_hessian
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the multivariate normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the multivariate normal likelihood function.
+
+        """
+        # H = function(q) {-matrix(c(-2*a -24*b*q[1]^2 +4*b*q[2] +4*b*q[4], 4*b*q[1], 0, 4*b*q[1], 0,
+        #                            4*b*q[1], -2*b*(1 +6*q[2]^2 -2*q[3]), 4*b*q[2], 0, 0,
+        #                            0, 4*b*q[2], -2*b, 0, 0,
+        #                            4*b*q[1], 0, 0, -2*b*(1 +6*q[4]^2 -2*q[5]), 4*b*q[4],
+        #                            0, 0, 0, 4*b*q[4], -2*b) ,d,d, byrow = T)}
+
+        log_likelihood_hessian = np.array([[-2*self.shape[0] - 24*self.shape[1]*position[0]**2 + 4*self.shape[1]*position[1]+4*self.shape[1]*position[3],
+                                            4*self.shape[1]*position[0],0,4*self.shape[1]*position[0],0],
+                                           [4*self.shape[1]*position[0],-2*self.shape[1]*(1 + 6*position[1]**2 - 2*position[2]),4*self.shape[1]*position[1],0,0],
+                                           [0,4*self.shape[1]*position[1],-2*self.shape[1],0,0],
+                                           [4*self.shape[1]*position[0],0,0,-2*self.shape[1]*(1 + 6*position[3]**2 - 2*position[4]),4*self.shape[1]*position[3]],
+                                           [0,0,0,4*self.shape[1]*position[3],-2*self.shape[1]]])
+        return log_likelihood_hessian
 
 def random_walk(model,number_of_samples,initial_position,step_size,proposal_covariance=np.eye(1),thinning_rate=1):
     """
@@ -274,7 +284,7 @@ def random_walk(model,number_of_samples,initial_position,step_size,proposal_cova
     ----------
 
     model : class
-        the model which the user wants to perform inference on. It should contain the method 'log_likelihood'.
+        the model which the user wants to perform inference on. It should contain the method 'log_likelihood'
 
     number_of_samples : integer
         the number of samples the random walk proposes
@@ -335,6 +345,7 @@ def random_walk(model,number_of_samples,initial_position,step_size,proposal_cova
 
         proposal_log_likelihood = model.log_likelihood(proposal)
 
+        # accept-reject step
         if(np.random.uniform() < np.exp(proposal_log_likelihood - current_log_likelihood)):
             current_position = proposal
             current_log_likelihood = proposal_log_likelihood
@@ -356,7 +367,7 @@ def mala(model,number_of_samples,initial_position,step_size,proposal_covariance=
     ----------
 
     model : class
-        the model which the user wants to perform inference on. It should contain the method 'log_likelihood'.
+        the model which the user wants to perform inference on. It should contain the method 'log_likelihood' and 'log_likelihood_gradient'
 
     number_of_samples : integer
         the number of samples the random walk proposes
@@ -369,7 +380,7 @@ def mala(model,number_of_samples,initial_position,step_size,proposal_covariance=
         should represent the covariance structure of the samples
 
     step size : double
-        a tuning parameter in the proposal step. this is a user defined parameter, change in order to get acceptance ratio ~0.234
+        a tuning parameter in the proposal step. this is a user defined parameter, change in order to get acceptance ratio ~0.5
 
     thinning_rate : integer
         the number of samples out of which you will keep one. this parameter can be increased to reduce autocorrelation if required
@@ -418,6 +429,7 @@ def mala(model,number_of_samples,initial_position,step_size,proposal_covariance=
         else:
             proposal = current_position + step_size*proposal_covariance.dot(current_log_likelihood_gradient)/2 + np.sqrt(step_size)*proposal_cholesky.dot(np.random.normal(size=number_of_parameters))
 
+        # compute transition probabilities for acceptance step
         proposal_log_likelihood = model.log_likelihood(proposal)
         proposal_log_likelihood_gradient = model.log_likelihood_gradient(proposal)
 
@@ -426,8 +438,8 @@ def mala(model,number_of_samples,initial_position,step_size,proposal_covariance=
 
         transition_kernel_pdf_forward = -np.transpose(forward_helper_variable).dot(proposal_covariance_inverse).dot(forward_helper_variable)/(2*step_size)
         transition_kernel_pdf_backward = -np.transpose(backward_helper_variable).dot(proposal_covariance_inverse).dot(backward_helper_variable)/(2*step_size)
-        # import pdb; pdb.set_trace()
 
+        # accept-reject step
         if(np.random.uniform() < np.exp(proposal_log_likelihood - transition_kernel_pdf_forward - current_log_likelihood + transition_kernel_pdf_backward)):
             current_position = proposal
             current_log_likelihood = proposal_log_likelihood
@@ -440,5 +452,105 @@ def mala(model,number_of_samples,initial_position,step_size,proposal_covariance=
     print("Acceptance ratio:",accepted_moves/number_of_iterations)
     return mcmc_samples
 
-def single_manifold_mala():
+def simple_manifold_mala(model,number_of_samples,initial_position,step_size,proposal_covariance=np.eye(1),thinning_rate=1):
+    """
+    Simple Manifold Metropolis adjusted Langevin algorithm which takes as input a model and returns a N x q matrix
+    of MCMC samples, where N is the number of samples and q is the number of parameters. Proposals, x', are drawn
+    centered from the current position, x, by x + h/2*proposal_covariance*log_likelihood_gradient +
+    h*sqrt(proposal_covariance)*normal(0,1), where h is the step_size
+
+    Parameters
+    ----------
+
+    model : class
+        the model which the user wants to perform inference on. It should contain the method 'log_likelihood'.
+
+    number_of_samples : integer
+        the number of samples the random walk proposes
+
+    initial_position : numpy array
+        starting value of the Markov chain
+
+    proposal_covariance: numpy array
+        a q x q matrix where q is the number of paramters in the model. For optimal sampling this
+        should represent the covariance structure of the samples
+
+    step size : double
+        a tuning parameter in the proposal step. this is a user defined parameter, change in order to get acceptance ratio ~0.5
+
+    thinning_rate : integer
+        the number of samples out of which you will keep one. this parameter can be increased to reduce autocorrelation if required
+
+    Returns
+    -------
+
+    mcmc_samples : numpy array
+        an N x q matrix of MCMC samples, where N is the number of samples and q is the number of parameters. These
+        are the accepted positions in parameter space
+
+    """
+    # initialise the covariance proposal matrix
+    number_of_parameters = len(initial_position)
+
+    # check if default value is used, and set to q x q identity
+    if np.array_equal(proposal_covariance, np.eye(1)):
+        proposal_covariance = np.eye(number_of_parameters)
+
+    if np.array_equal(proposal_covariance, np.eye(number_of_parameters)):
+        identity = True
+    else:
+        identity = False
+        proposal_cholesky = np.linalg.cholesky(proposal_covariance)
+
+    proposal_covariance_inverse = np.linalg.inv(proposal_covariance)
+
+    # initialise samples matrix and acceptance ratio counter
+    accepted_moves = 0
+    mcmc_samples = np.zeros((number_of_samples,number_of_parameters))
+    mcmc_samples[0] = initial_position
+    number_of_iterations = number_of_samples*thinning_rate
+
+    # initial markov chain
+    current_position = initial_position
+    current_log_likelihood = model.log_likelihood(current_position)
+    current_log_likelihood_gradient = model.log_likelihood_gradient(current_position)
+    current_log_likelihood_hessian = -model.log_likelihood_hessian(current_position)
+    # import pdb; pdb.set_trace()
+    current_hessian_cholesky = np.linalg.cholesky(current_log_likelihood_hessian)
+
+    for iteration_index in range(1,number_of_iterations):
+        # progress measure
+        if iteration_index%(number_of_iterations//10)==0:
+            print("Progress: ",100*iteration_index//number_of_iterations,'%')
+
+        if identity:
+            proposal = current_position + (step_size*current_log_likelihood_hessian.dot(current_log_likelihood_gradient)/2 +
+                                           np.sqrt(step_size)*current_hessian_cholesky.dot(np.random.normal(size=number_of_parameters)))
+        else:
+            proposal = current_position + (step_size*proposal_covariance.dot(current_log_likelihood_hessian).dot(current_log_likelihood_gradient)/2 +
+                                           np.sqrt(step_size)*proposal_cholesky.dot(current_hessian_cholesky).dot(np.random.normal(size=number_of_parameters)))
+
+        proposal_log_likelihood = model.log_likelihood(proposal)
+        proposal_log_likelihood_gradient = model.log_likelihood_gradient(proposal)
+        proposal_log_likelihood_hessian = -model.log_likelihood_hessian(proposal)
+        # import pdb; pdb.set_trace()
+        proposal_hessian_cholesky = np.linalg.cholesky(proposal_log_likelihood_hessian)
+
+        forward_helper_variable = proposal - current_position - step_size*proposal_covariance.dot(current_log_likelihood_hessian).dot(current_log_likelihood_gradient)/2
+        backward_helper_variable = current_position - proposal - step_size*proposal_covariance.dot(proposal_log_likelihood_hessian).dot(proposal_log_likelihood_gradient)/2
+
+        transition_kernel_pdf_forward = -np.transpose(forward_helper_variable).dot(proposal_covariance_inverse).dot(forward_helper_variable)/(2*step_size)
+        transition_kernel_pdf_backward = -np.transpose(backward_helper_variable).dot(proposal_covariance_inverse).dot(backward_helper_variable)/(2*step_size)
+
+        if(np.random.uniform() < np.exp(proposal_log_likelihood - transition_kernel_pdf_forward - current_log_likelihood + transition_kernel_pdf_backward)):
+            current_position = proposal
+            current_log_likelihood = proposal_log_likelihood
+            current_log_likelihood_gradient = proposal_log_likelihood_gradient
+            current_log_likelihood_hessian = proposal_log_likelihood_hessian
+            accepted_moves += 1
+
+        if iteration_index%thinning_rate == 0:
+            mcmc_samples[np.int(iteration_index/thinning_rate)] = current_position
+
+    print("Acceptance ratio:",accepted_moves/number_of_iterations)
     return mcmc_samples
--- test/test_inference.py
+++ test/test_inference.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 font = {'size'   : 10}
 plt.rc('font', **font)
 import numpy as np
+from scipy import stats
 import multiprocessing as mp
 import multiprocessing.pool as mp_pool
 # make sure we find the right python module
@@ -46,6 +47,13 @@ class TestInference(unittest.TestCase):
         # test that we get the expected number of samples
         np.testing.assert_almost_equal(output.shape[0],number_of_samples)
 
+        plt.clf()
+        x = np.linspace(mean - 3*np.sqrt(variance), mean + 3*np.sqrt(variance), 100)
+        h = plt.plot(x, stats.norm.pdf(x, mean, np.sqrt(variance)),zorder=1)
+        myhist = plt.hist(output,100,density=True)
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','1d_normal_random_walk_histogram.pdf'))
+
     def xest_random_walk_on_multivariate_normal(self):
         print("\nTesting random walk on a multivariate normal distribution")
         mean = np.array([6,2,18])
@@ -75,6 +83,7 @@ class TestInference(unittest.TestCase):
         np.testing.assert_almost_equal(output.shape[1],len(initial_position))
 
         # plot a pairgrid
+        plt.clf()
         g = sns.PairGrid(pd.DataFrame(output[:-1:100],columns=['$x_1$','$x_2$','$x_3$']))
         g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
         g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
@@ -83,7 +92,7 @@ class TestInference(unittest.TestCase):
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','pair_grid_random_walk_multivariate_normal.pdf'))
 
-    def test_random_walk_on_hybrid_rosenbrock23(self):
+    def xest_random_walk_on_hybrid_rosenbrock23(self):
         print("\nTesting random walk on a hybrid Rosenbrock (2,3) distribution")
         mean = 1
         shape = np.array([.05,5,5,5,5])
@@ -145,22 +154,12 @@ class TestInference(unittest.TestCase):
         # test that we get the expected number of samples
         np.testing.assert_almost_equal(output.shape[0],number_of_samples)
 
-        from scipy import stats
         plt.clf()
         x = np.linspace(mean - 3*np.sqrt(variance), mean + 3*np.sqrt(variance), 100)
         h = plt.plot(x, stats.norm.pdf(x, mean, np.sqrt(variance)),zorder=1)
         myhist = plt.hist(output,100,density=True)
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','1d_normal_mala_histogram.pdf'))
-
-        # plot a pairgrid
-        # g = sns.PairGrid(pd.DataFrame(output[:-1:10],columns=['$x_1$','$x_2$','$x_3$','$x_4$','$x_5$']))
-        # g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
-        # g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
-        # g = g.map_diag(sns.distplot,color='#20948B')
-        # plt.tight_layout()
-        # plt.savefig(os.path.join(os.path.dirname(__file__),
-        #                          'output','pair_grid.pdf'))
 
     def xest_mala_on_multivariate_normal(self):
         print("\nTesting MALA on a multivariate normal distribution")
@@ -191,6 +190,7 @@ class TestInference(unittest.TestCase):
         np.testing.assert_almost_equal(output.shape[1],len(initial_position))
 
         # plot a pairgrid
+        plt.clf()
         g = sns.PairGrid(pd.DataFrame(output[:-1:100],columns=['$x_1$','$x_2$','$x_3$']))
         g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
         g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
@@ -198,3 +198,111 @@ class TestInference(unittest.TestCase):
         plt.tight_layout()
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','pair_grid_mala_multivariate_normal.pdf'))
+
+    def xest_mala_on_hybrid_rosenbrock23(self):
+        print("\nTesting MALA on a hybrid Rosenbrock (2,3) distribution")
+        mean = 1
+        shape = np.array([.05,5,5,5,5])
+
+        rosenbrock_test = covid_inference.hybrid_rosenbrock23(mean,shape)
+        number_of_samples = 1000000
+        initial_position = np.array([1.0,1.0,1.0,1.0,1.0])
+        step_size = 0.07
+        # proposal_covariance = np.array([[1,0.5],[0.5,1]])
+        thinning_rate = 1
+
+        output = covid_inference.mala(rosenbrock_test,
+                                      number_of_samples,
+                                      initial_position,
+                                      step_size,
+                                      thinning_rate=thinning_rate)
+
+        # test that mean and variance inferences are within 0.01 of the ground truth
+        # np.testing.assert_allclose(np.mean(output[:,0]),mean,0.1)
+
+        # test that we get the expected number of samples
+        np.testing.assert_almost_equal(output.shape[0],number_of_samples)
+        np.testing.assert_almost_equal(output.shape[1],len(initial_position))
+
+        # plot a pairgrid
+        plt.clf()
+        g = sns.PairGrid(pd.DataFrame(output[:-1:10],columns=['$x_1$','$x_2$','$x_3$','$x_4$','$x_5$']))
+        g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
+        g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
+        g = g.map_diag(sns.distplot,color='#20948B')
+        plt.tight_layout()
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','pair_grid_mala_rosenbrock.pdf'))
+
+    def xest_simple_manifold_mala_on_normal(self):
+        print("\nTesting simple manifold MALA on a normal distribution")
+        mean = np.array([6,2,18])
+        covariance_matrix = np.eye(3)
+
+        multivariate_normal_test = covid_inference.multivariate_normal(mean,covariance_matrix)
+        number_of_samples = 100000
+        initial_position = np.array([1.0,1.0,1.0])
+        step_size = 1.5
+        # proposal_covariance = np.array([[1,0.5],[0.5,1]])
+        thinning_rate = 1
+
+        output = covid_inference.simple_manifold_mala(multivariate_normal_test,
+                                             number_of_samples,
+                                             initial_position,
+                                             step_size,
+                                             thinning_rate=thinning_rate)
+
+        # test that mean and variance inferences are within 0.01 of the ground truth
+        np.testing.assert_allclose(np.mean(output[:,0]),mean[0],0.1)
+        np.testing.assert_allclose(np.var(output[:,0]),covariance_matrix[0,0],0.1)
+        np.testing.assert_allclose(np.mean(output[:,1]),mean[1],0.1)
+        np.testing.assert_allclose(np.var(output[:,1]),covariance_matrix[1,1],0.1)
+
+        # test that we get the expected number of samples
+        np.testing.assert_almost_equal(output.shape[0],number_of_samples)
+        np.testing.assert_almost_equal(output.shape[1],len(initial_position))
+
+        # plot a pairgrid
+        plt.clf()
+        g = sns.PairGrid(pd.DataFrame(output[:-1:10],columns=['$x_1$','$x_2$','$x_3$']))
+        g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
+        g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
+        g = g.map_diag(sns.distplot,color='#20948B')
+        plt.tight_layout()
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','pair_grid_sm_mala_multivariate_normal.pdf'))
+
+    def test_simple_manifold_mala_on_rosenbrock(self):
+        print("\nTesting smMALA on a hybrid Rosenbrock (2,3) distribution")
+        mean = 1
+        shape = np.array([.05,5,5,5,5])
+
+        rosenbrock_test = covid_inference.hybrid_rosenbrock23(mean,shape)
+        number_of_samples = 100000
+        initial_position = np.array([1.0,1.0,1.0,1.0,1.0])
+        step_size = 0.007
+        # proposal_covariance = np.array([[1,0.5],[0.5,1]])
+        thinning_rate = 1
+
+        output = covid_inference.simple_manifold_mala(rosenbrock_test,
+                                      number_of_samples,
+                                      initial_position,
+                                      step_size,
+                                      thinning_rate=thinning_rate)
+
+        # test that mean and variance inferences are within 0.01 of the ground truth
+        # np.testing.assert_allclose(np.mean(output[:,0]),mean,0.1)
+
+        # test that we get the expected number of samples
+        np.testing.assert_almost_equal(output.shape[0],number_of_samples)
+        np.testing.assert_almost_equal(output.shape[1],len(initial_position))
+
+        # plot a pairgrid
+        plt.clf()
+        g = sns.PairGrid(pd.DataFrame(output[:-1:10],columns=['$x_1$','$x_2$','$x_3$','$x_4$','$x_5$']))
+        g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
+        g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
+        g = g.map_diag(sns.distplot,color='#20948B')
+        plt.tight_layout()
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','pair_grid_sm_mala_rosenbrock.pdf'))
