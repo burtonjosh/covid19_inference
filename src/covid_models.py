00a2f4900c89c8fc175485055430f8f18b8789b4
Josh Burton <josh@burton-online.me.uk>
2020-04-13 19:23:07 +0100

    models for poisson and negative binomial data

---
+++ src/covid_models.py
@@ -0,0 +1,463 @@
+import numpy as np
+import scipy.stats as st
+from scipy.special import factorial
+
+class normal:
+    """
+    A one dimensional normal distribution with user specified mean and variance. Can be used as a
+    target distribution in MCMC methods.
+    """
+    def __init__(self,mean,variance):
+        """
+        Constructor function for the normal class.
+
+        Parameters
+        ----------
+
+        mean : double
+            the mean of the normal distribution
+
+        variance : double
+            the variance of the normal distribution
+        """
+        self.mean = mean
+        self.variance = variance
+
+    def log_likelihood(self, position):
+        """
+        A function returning the log likelihood of the normal distribution for a given position in a
+        Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood : double
+            the log of the normal likelihood function.
+
+        """
+        log_likelihood = -np.power((position-self.mean),2)/(2*self.variance)
+        return log_likelihood
+
+    def log_likelihood_gradient(self,position):
+        """
+        A function returning the gradient of the log likelihood of the normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_gradient : double
+            the gradient of the log of the normal likelihood function.
+
+        """
+        log_likelihood_gradient = -(position-self.mean)/(self.variance)
+        return log_likelihood_gradient
+
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the normal likelihood function.
+
+        """
+        log_likelihood_hessian = -1/self.variance
+        return log_likelihood_hessian
+
+class multivariate_normal:
+    """
+    A multi dimensional normal distribution with user specified mean and symmetric, positive definite,
+    covariance matrix. Can be used as a target distribution in MCMC methods.
+    """
+    def __init__(self,mean,covariance_matrix):
+        """
+        Constructor function for the multivariate normal class. This computes the covariance matrix inverse for
+        use in the subsequent methods.
+
+        Parameters
+        ----------
+
+        mean : numpy array
+            the mean vector of the multivariate normal distribution
+
+        covariance_matrix : numpy array
+            the covariance matrix of the multivariate normal distribution
+        """
+        self.mean = mean
+        self.covariance_matrix = covariance_matrix
+        self.covariance_matrix_inverse = np.linalg.inv(covariance_matrix)
+
+    def log_likelihood(self, position):
+        """
+        A function returning the log likelihood of the multivariate normal distribution for a given position in a
+        Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood : double
+            the log of the multivariate normal likelihood function.
+
+        """
+        log_likelihood = -0.5*np.transpose(position-self.mean).dot(self.covariance_matrix_inverse).dot(position-self.mean)
+        return log_likelihood
+
+    def log_likelihood_gradient(self,position):
+        """
+        A function returning the gradient of the log likelihood of the multivariate normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_gradient : double
+            the gradient of the log of the multivariate normal likelihood function.
+
+        """
+        log_likelihood_gradient = -self.covariance_matrix_inverse.dot(position-self.mean)
+        return log_likelihood_gradient
+
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the multivariate normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the multivariate normal likelihood function.
+
+        """
+        log_likelihood_hessian = -self.covariance_matrix_inverse
+        return log_likelihood_hessian
+
+class hybrid_rosenbrock23:
+    """
+    A multi dimensional distribution, detailed by Pagani (2019). See https://arxiv.org/abs/1903.09556 .
+    Here n_1 = 3 and n_2 = 2. Can be used as a target distribution in MCMC methods.
+    """
+    def __init__(self,mean,shape):
+        """
+        Constructor function for the hybrid rosenbrock class.
+
+        Parameters
+        ----------
+
+        mean : double
+            the mean, mu, of the hybrid rosenbrock distribution
+
+        shape : numpy array
+            a 1x5 vector containing one half the precision of each normal kernel
+
+        """
+        self.mean = mean
+        self.shape = shape
+
+    def log_likelihood(self, position):
+        """
+        A function returning the log likelihood of the hybrid rosenbrock distribution for a given position in a
+        Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            a 5x1 numpy array giving the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood : double
+            the log of the hybrid rosenbrock likelihood function.
+
+        """
+        log_likelihood = -(self.shape[0]*np.power(position[0]-self.mean,2) +
+                           self.shape[1]*np.power(position[1]-np.power(position[0],2),2) +
+                           self.shape[2]*np.power(position[2]-np.power(position[1],2),2) +
+                           self.shape[3]*np.power(position[3]-np.power(position[0],2),2) +
+                           self.shape[4]*np.power(position[4]-np.power(position[3],2),2))
+        return log_likelihood
+
+    def log_likelihood_gradient(self,position):
+        """
+        A function returning the gradient of the log likelihood of the hybrid rosenbrock distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_gradient : double
+            the gradient of the log of the multivariate normal likelihood function.
+
+        """
+        log_likelihood_gradient = np.array([2*self.shape[0]*self.mean - 8*self.shape[1]*position[0]**3 + position[0]*(-2*self.shape[0] + 4*self.shape[1]*position[1] +4*self.shape[1]*position[3]),
+                                            2*self.shape[1]*(position[0]**2 +position[1]*(-1 -2*position[1]**2 +2*position[2]) ),
+                                            2*self.shape[2]*(position[1]**2 -position[2]),
+                                            2*self.shape[3]*(position[0]**2 +position[3]*(-1 -2*position[3]**2 +2*position[4]) ),
+                                            2*self.shape[4]*(position[3]**2 -position[4])])
+        return log_likelihood_gradient
+
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the multivariate normal distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the multivariate normal likelihood function.
+
+        """
+        log_likelihood_hessian = np.array([[-2*self.shape[0] - 24*self.shape[1]*position[0]**2 + 4*self.shape[1]*position[1]+4*self.shape[1]*position[3],
+                                             4*self.shape[1]*position[0],0,4*self.shape[1]*position[0],0],
+                                            [4*self.shape[1]*position[0],-2*self.shape[1]*(1 + 6*position[1]**2 - 2*position[2]),4*self.shape[1]*position[1],0,0],
+                                            [0,4*self.shape[1]*position[1],-2*self.shape[1],0,0],
+                                            [4*self.shape[1]*position[0],0,0,-2*self.shape[1]*(1 + 6*position[3]**2 - 2*position[4]),4*self.shape[1]*position[3]],
+                                            [0,0,0,4*self.shape[1]*position[3],-2*self.shape[1]]])
+        return log_likelihood_hessian
+
+class negative_binomial_data:
+    """
+
+    """
+    def __init__(self,data):
+        """
+        Constructor function for the negative binomial class for data.
+
+        Parameters
+        ----------
+
+        data : numpy array
+            a 1xn array containing number of new cases per day for a region
+
+        """
+        self.data = data
+
+    def log_likelihood(self, position):
+        """
+        A function returning the log likelihood of the negative binomial distribution for a given position in a
+        Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            a 3x1 numpy array giving the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood : double
+            the log of the hybrid rosenbrock likelihood function.
+
+        """
+        if np.any(position < 0):
+            return -np.inf
+        else:
+            day_numbers = np.arange(0,len(self.data))
+            mean = position[0]*np.exp(position[1]*day_numbers)
+            variance = position[2]*np.ones(len(day_numbers))
+            r = mean/(variance-1)
+            p = 1/variance
+            log_likelihood = np.sum(st.nbinom.logpmf(self.data,r,p))
+            return log_likelihood
+
+    def log_likelihood_gradient(self,position):
+        """
+        A function returning the gradient of the log likelihood of the negative binomial distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_gradient : double
+            the gradient of the log of the multivariate normal likelihood function.
+
+        """
+        pass
+        # day_numbers = np.arange(0,len(data))
+        # mean = position[0]*np.exp(position[1]*day_numbers)
+        # variance = position[2]*np.ones(len(day_numbers))
+        # r = mean/(variance-1)
+        # p = 1/variance
+        # log_likelihood = np.sum(st.nbinom.logpmf(self.data,r,p))
+        # return log_likelihood_gradient
+
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the negative binomial distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the multivariate normal likelihood function.
+
+        """
+        pass
+        # return log_likelihood_hessian
+
+class poisson_data:
+    """
+
+    """
+    def __init__(self,data,days):
+        """
+        Constructor function for the poisson class for data.
+
+        Parameters
+        ----------
+
+        data : numpy array
+            a 1xn array containing number of new cases per day for a region. The first column is days, second column
+            is the cases.
+
+        days : numpy array
+            a 2xn array containing the days. The first row are all 1's, the second row are the days in order.
+
+        """
+        self.data = data
+        self.days = days
+
+    def log_likelihood(self, position):
+        """
+        A function returning the log likelihood of the negative binomial distribution for a given position in a
+        Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            a 3x1 numpy array giving the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood : double
+            the log of the hybrid rosenbrock likelihood function.
+
+        """
+        if np.any(position < 0):
+            return -np.inf
+        else:
+            # day_numbers = np.arange(0,len(self.data))
+            # mean = position[0]*np.exp(position[1]*day_numbers)
+            # r = mean/(variance-1)
+            # p = 1/variance
+            log_likelihood = np.sum(self.data*(position.dot(self.days.T)) - np.exp(position.dot(self.days.T)) - np.log(factorial(self.data)))
+            return log_likelihood
+
+    def log_likelihood_gradient(self,position):
+        """
+        A function returning the gradient of the log likelihood of the negative binomial distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_gradient : double
+            the gradient of the log of the multivariate normal likelihood function.
+
+        """
+        pass
+        # day_numbers = np.arange(0,len(data))
+        # mean = position[0]*np.exp(position[1]*day_numbers)
+        # variance = position[2]*np.ones(len(day_numbers))
+        # r = mean/(variance-1)
+        # p = 1/variance
+        # log_likelihood = np.sum(st.nbinom.logpmf(self.data,r,p))
+        # return log_likelihood_gradient
+
+    def log_likelihood_hessian(self,position):
+        """
+        A function returning the Hessian of the log likelihood of the negative binomial distribution
+        for a given position in a Markov chain.
+
+        Parameters
+        ----------
+
+        position : numpy array
+            the current position in the Markov chain.
+
+        Returns
+        -------
+
+        log_likelihood_hessian : double
+            the Hessian of the log of the multivariate normal likelihood function.
+
+        """
+        pass
+        # return log_likelihood_hessian
--- test/test_inference.py
+++ test/test_inference.py
@@ -16,7 +16,8 @@ import multiprocessing as mp
 import multiprocessing.pool as mp_pool
 # make sure we find the right python module
 sys.path.append(os.path.join(os.path.dirname(__file__),'..','src'))
-import covid_inference
+
+import covid_inference; import covid_models
 
 number_of_cpus = mp.cpu_count()
 
@@ -27,7 +28,7 @@ class TestInference(unittest.TestCase):
         mean = 7
         variance = 2
 
-        normal_test = covid_inference.normal(mean,variance)
+        normal_test = covid_models.normal(mean,variance)
         number_of_samples = 10000
         initial_position = np.array([1.0])
         step_size = 7
@@ -59,7 +60,7 @@ class TestInference(unittest.TestCase):
         mean = np.array([6,2,18])
         covariance_matrix = np.eye(3)
 
-        multivariate_normal_test = covid_inference.multivariate_normal(mean,covariance_matrix)
+        multivariate_normal_test = covid_models.multivariate_normal(mean,covariance_matrix)
         number_of_samples = 100000
         initial_position = np.array([1.0,1.0,1.0])
         step_size = 1.5
@@ -97,7 +98,7 @@ class TestInference(unittest.TestCase):
         mean = 1
         shape = np.array([.05,5,5,5,5])
 
-        rosenbrock_test = covid_inference.hybrid_rosenbrock23(mean,shape)
+        rosenbrock_test = covid_models.hybrid_rosenbrock23(mean,shape)
         number_of_samples = 1000000
         initial_position = np.array([1.0,1.0,1.0,1.0,1.0])
         step_size = 0.2
@@ -132,7 +133,7 @@ class TestInference(unittest.TestCase):
         mean = 7
         variance = 2
 
-        normal_test = covid_inference.normal(mean,variance)
+        normal_test = covid_models.normal(mean,variance)
         number_of_samples = 100000
         initial_position = np.array([1.0])
         step_size = 4.0
@@ -166,7 +167,7 @@ class TestInference(unittest.TestCase):
         mean = np.array([6,2,18])
         covariance_matrix = np.eye(3)
 
-        multivariate_normal_test = covid_inference.multivariate_normal(mean,covariance_matrix)
+        multivariate_normal_test = covid_models.multivariate_normal(mean,covariance_matrix)
         number_of_samples = 100000
         initial_position = np.array([1.0,1.0,1.0])
         step_size = 1.5
@@ -204,7 +205,7 @@ class TestInference(unittest.TestCase):
         mean = 1
         shape = np.array([.05,5,5,5,5])
 
-        rosenbrock_test = covid_inference.hybrid_rosenbrock23(mean,shape)
+        rosenbrock_test = covid_models.hybrid_rosenbrock23(mean,shape)
         number_of_samples = 1000000
         initial_position = np.array([1.0,1.0,1.0,1.0,1.0])
         step_size = 0.07
@@ -234,12 +235,12 @@ class TestInference(unittest.TestCase):
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','pair_grid_mala_rosenbrock.pdf'))
 
-    def test_simple_manifold_mala_on_multivariate_normal(self):
+    def xest_simple_manifold_mala_on_multivariate_normal(self):
         print("\nTesting simple manifold MALA on a multivariate normal distribution")
         mean = np.array([6,2,18])
         covariance_matrix = np.eye(3)
 
-        multivariate_normal_test = covid_inference.multivariate_normal(mean,covariance_matrix)
+        multivariate_normal_test = covid_models.multivariate_normal(mean,covariance_matrix)
         number_of_samples = 1000
         initial_position = np.array([5.0,1.0,13.0])
         step_size = 3.5
@@ -254,6 +255,9 @@ class TestInference(unittest.TestCase):
                                              thinning_rate=thinning_rate)
 
         # test that mean and variance inferences are within 0.01 of the ground truth
+        print('mean 1',np.mean(output[:,0]))
+        print('mean 2',np.mean(output[:,1]))
+        print('mean 3',np.mean(output[:,2]))
         np.testing.assert_allclose(np.mean(output[:,0]),mean[0],0.1)
         np.testing.assert_allclose(np.var(output[:,0]),covariance_matrix[0,0],0.1)
         np.testing.assert_allclose(np.mean(output[:,1]),mean[1],0.1)
@@ -275,16 +279,24 @@ class TestInference(unittest.TestCase):
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','pair_grid_sm_mala_multivariate_normal.pdf'))
 
+        plt.clf()
+        for i in range(3):
+            x = np.linspace(mean[i] - 3*np.sqrt(covariance_matrix[i,i]), mean[i] + 3*np.sqrt(covariance_matrix[i,i]), 100)
+            h = plt.plot(x, stats.norm.pdf(x, mean[i], np.sqrt(covariance_matrix[i,i])),zorder=i)
+        plt.hist(output,100,density=True)
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','multivariate_normal_random_walk_histogram.pdf'))
+
     def xest_simple_manifold_mala_on_rosenbrock(self):
         print("\nTesting smMALA on a hybrid Rosenbrock (2,3) distribution")
         mean = 1
         shape = np.array([.05,5,5,5,5])
 
-        rosenbrock_test = covid_inference.hybrid_rosenbrock23(mean,shape)
+        rosenbrock_test = covid_models.hybrid_rosenbrock23(mean,shape)
         number_of_samples = 100000
         initial_position = np.array([1.0,1.0,1.0,1.0,1.0])
-        step_size = 0.2
-        thinning_rate = 10
+        step_size = 0.32
+        thinning_rate = 1
 
         output = covid_inference.simple_manifold_mala(rosenbrock_test,
                                                       number_of_samples,
@@ -293,6 +305,7 @@ class TestInference(unittest.TestCase):
                                                       thinning_rate=thinning_rate,)
 
         # test that mean and variance inferences are within 0.01 of the ground truth
+        print(np.mean(output[:,0]))
         # np.testing.assert_allclose(np.mean(output[:,0]),mean,0.1)
 
         # test that we get the expected number of samples
@@ -308,3 +321,101 @@ class TestInference(unittest.TestCase):
         plt.tight_layout()
         plt.savefig(os.path.join(os.path.dirname(__file__),
                                  'output','pair_grid_sm_mala_rosenbrock.pdf'))
+
+        plt.clf()
+        x = np.linspace(mean - 3*np.sqrt(0.1), mean + 3*np.sqrt(0.1), 100)
+        h = plt.plot(x, stats.norm.pdf(x, mean, np.sqrt(0.1)))
+        plt.hist(output[:,0],100,density=True)
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','rosenbrock_sm_mala_histogram.pdf'))
+
+    def xest_random_walk_on_poisson(self):
+        print("\nTesting random walk on a poisson distribution with data")
+
+        # make some data
+        n = 100
+        beta = np.array([1,2])
+        days = np.zeros((n,len(beta)))
+        days[:,0] = 1
+        days[:,1] = np.arange(0,1,.01)
+        mu = np.exp(days.dot(beta))
+        data = stats.poisson.rvs(mu)
+
+        poisson_test = covid_models.poisson_data(data,days)
+        number_of_samples = 100000
+        initial_position = np.array([0.92784717, 2.13682143])
+        step_size = 0.1
+        # proposal_covariance = np.diag([20.0,0.02,20.0])
+        # proposal_covariance = np.array([[1,0.5],[0.5,1]])
+        thinning_rate = 1
+
+        output = covid_inference.random_walk(poisson_test,
+                                             number_of_samples,
+                                             initial_position,
+                                             step_size,
+                                             thinning_rate=thinning_rate)
+
+        # test that mean and variance inferences are within 0.01 of the ground truth
+        # np.testing.assert_allclose(np.mean(output[:,0]),mean[0],0.1)
+        # np.testing.assert_allclose(np.var(output[:,0]),covariance_matrix[0,0],0.1)
+
+        # test that we get the expected number of samples
+        # np.testing.assert_almost_equal(output.shape[0],number_of_samples)
+        # np.testing.assert_almost_equal(output.shape[1],len(initial_position))
+        print('x_1',np.mean(output[:,0]))
+        print('x_2',np.mean(output[:,1]))
+        # plot a pairgrid
+        plt.clf()
+        g = sns.PairGrid(pd.DataFrame(output,columns=['$x_1$','$x_2$']))
+        g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
+        g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
+        g = g.map_diag(sns.distplot,color='#20948B')
+        plt.tight_layout()
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','pair_grid_random_walk_poisson.pdf'))
+
+    def test_random_walk_on_negative_binomial(self):
+        print("\nTesting random walk on a negative binomial distribution with data")
+
+        # make some data
+        n = 24
+        beta = np.array([1,2])
+        days = np.zeros((n,len(beta)))
+        days[:,0] = 1
+        days[:,1] = np.linspace(0,1,24)
+        mu = np.exp(days.dot(beta))
+        data = stats.poisson.rvs(mu)
+
+        negative_binomial_test = covid_models.negative_binomial_data(data)
+        number_of_samples = 10000
+        initial_position = np.array([1, 2, 2])
+        step_size = 0.01
+        # proposal_covariance = np.diag([20.0,0.02,20.0])
+        # proposal_covariance = np.array([[1,0.5],[0.5,1]])
+        thinning_rate = 1
+
+        output = covid_inference.random_walk(negative_binomial_test,
+                                             number_of_samples,
+                                             initial_position,
+                                             step_size,
+                                             thinning_rate=thinning_rate)
+
+        # test that mean and variance inferences are within 0.01 of the ground truth
+        # np.testing.assert_allclose(np.mean(output[:,0]),mean[0],0.1)
+        # np.testing.assert_allclose(np.var(output[:,0]),covariance_matrix[0,0],0.1)
+
+        # test that we get the expected number of samples
+        # np.testing.assert_almost_equal(output.shape[0],number_of_samples)
+        # np.testing.assert_almost_equal(output.shape[1],len(initial_position))
+        print('x_1',np.mean(output[:,0]))
+        print('x_2',np.mean(output[:,1]))
+        print('x_3',np.mean(output[:,2]))
+        # plot a pairgrid
+        plt.clf()
+        g = sns.PairGrid(pd.DataFrame(output,columns=['$x_1$','$x_2$','$x_3$']))
+        g = g.map_upper(sns.scatterplot,size=2,color='#20948B')
+        g = g.map_lower(sns.kdeplot,color="#20948B",shade=True,shade_lowest=False)
+        g = g.map_diag(sns.distplot,color='#20948B')
+        plt.tight_layout()
+        plt.savefig(os.path.join(os.path.dirname(__file__),
+                                 'output','pair_grid_random_walk_negative_binomial.pdf'))
