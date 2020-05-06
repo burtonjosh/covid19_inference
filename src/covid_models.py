a0adec7b4a5fd5dbd354da6323eee2463dc5ea8f
Josh Burton <josh@burton-online.me.uk>
2020-05-06 11:28:05 +0100

    changing default fit

--- src/covid_models.py
+++ src/covid_models.py
@@ -440,7 +440,10 @@ class delayed_compartment_model:
     """
     # TODO:
     """
-    def __init__(self,region='EN',fit=['hospital_incidence']):
+    def __init__(self,region='EN',fit=['hospital_incidence',
+                                       'hospital_prevalence',
+                                       'icu_prevalence',
+                                       'death_incidence']):
         """
         Constructor function for the delayed_compartment_model class.
 
@@ -901,7 +904,7 @@ class delayed_compartment_model:
                 log_likelihood += np.sum(st.nbinom.logpmf(self.death_data,
                                                           (rHR*Yall[20,self.death_indices] + rCD*Yall[9,self.death_indices])/(sigma_0-1),
                                                           1/sigma_0))
-            # import pdb; pdb.set_trace()
+            import pdb; pdb.set_trace()
             return log_likelihood
 
     def log_likelihood_gradient(self,position):
